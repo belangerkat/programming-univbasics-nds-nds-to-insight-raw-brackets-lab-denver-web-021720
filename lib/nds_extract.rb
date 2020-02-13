20f5ed09b2df359be30630b079963fbf3783c498
Katie Belanger <belangerkat@gmail.com>
2020-02-13 21:12:52 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -8,15 +8,16 @@ def directors_totals(nds)
   directors_index = 0
   totals = {}
 
-  while directors_index < nds.count do
+  while directors_index < nds.length do
     director_name = nds[directors_index][:name]
     totals[director_name] = 0
     movie_index = 0
 
-    while movie_index < nds[directors_index][:movies].count do
+    while movie_index < nds[directors_index][:movies].length do
       totals [director_name] += nds[directors_index][:movies][movie_index][:worldwide_gross]
       movie_index += 1
     end
+
     directors_index += 1
   end
   totals
